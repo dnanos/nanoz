4e5f24c7bcfd7b6b9a0addba4a5d6a0f1d945c7e
dnanos <nanos@mail.by>
2013-12-25 19:19:32 +0300

    fix: heroku troubles

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,3 @@
 class Book < ActiveRecord::Base
   belongs_to :publisher
-  
-  validates :name, :presence => true, :uniqueness => true
-  validates_presence_of[:year, :pages, :weight]
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,5 +1,3 @@
 class Publisher < ActiveRecord::Base
   has_many :book
-
-  validates :name, :presence => true, :uniqueness => true
 end
