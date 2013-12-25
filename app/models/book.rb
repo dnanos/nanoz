c390d02d7ae9d2e40ac1ad82336db93cf9fb5337
dnanos <nanos@mail.by>
2013-12-25 18:38:19 +0300

    fix: routes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
   belongs_to :publisher
+  
+  validates :name, :presence => true, :uniqueness => true
+  validates_presence_of[:year, :pages, :weight]
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,2 +1,5 @@
 class Publisher < ActiveRecord::Base
+  has_many :book
+
+  validates :name, :presence => true, :uniqueness => true
 end
