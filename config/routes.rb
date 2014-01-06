fca2683064d2e4ebf7a15750b59c81bd6bb6bf66
dnanos <nanos@mail.by>
2014-01-06 18:20:51 +0300

    v 0.0.05-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Nanoz::Application.routes.draw do
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, :path_prefix => 'd'
 
   resources :books
   resources :publishers
-
+  resources :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
